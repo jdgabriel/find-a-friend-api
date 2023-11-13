b6ec48fc4e2b30ddcaaccad926cf39df18c4760b
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2023-11-13 13:51:41 -0300

    define core application

---
+++ src/core/entities/unique-entity-id.ts
@@ -0,0 +1,17 @@
+import { randomUUID } from 'node:crypto'
+
+export class UniqueEntityID {
+  private _value: string
+
+  constructor(id?: string) {
+    this._value = id ?? randomUUID()
+  }
+
+  get value() {
+    return this._value
+  }
+
+  public equals(id: UniqueEntityID) {
+    return id._value === this._value
+  }
+}
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tsConfigPaths()],
+  test: {
+    globals: true,
+  },
+})
